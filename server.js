97be2c5797a7e04967725e119219142c776787e5
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-06-30 01:33:35 +0200

    Created a default routehandler
    Just a default route, no error handling or specifics

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const port = process.env.PORT || 8080;
 const auth = require('./routes/api/auth');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
+const defaultRoutes = require('./routes/default');
 
 app.use('/api/auth', auth);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
+app.use('*', defaultRoutes);
 // -------------------------------------
 
 app.listen(port, () => console.log(`Server live on port: ${ port }`));
