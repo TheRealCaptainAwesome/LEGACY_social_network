78e8d9247fb9b3a163cf1d7f47f81d9675a43375
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-01 03:05:25 +0200

    Saving changes
    No need for branch atm

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
 
-router.get('/placeholder', (req, res) => res.json({ msg: 'authroutes works' }));
+router.post('/register', (req, res) => {
+    User.findOne({ email: req.body.email })
+    .then(
+        user => {
+            if(user) {
+                return res.json({ message: 'This email already exist.' })
+            } else {
+                const createUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                });
+            }
+        }
+    );
+} );
 
 module.exports = router;
