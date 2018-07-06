6367388bba1df1a84334d8bc94f05d27f7dc3d3c
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-06 02:28:17 +0200

    Formed the response on secure route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -86,7 +86,7 @@ router.post('/login', (req, res) => {
 }); // END of /login
 
 router.get('/secure', passport.authenticate('jwt', { session: false } ), (req, res) => {
-    res.json({ message: 'success!' });
+    res.json({ id: req.user.id, name: req.user.name, email: req.user.email});
 });
 
 module.exports = router;
