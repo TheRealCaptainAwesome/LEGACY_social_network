f4c424a7ae477a86660daa1a63cb6872930859eb
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-08 04:06:45 +0200

    Worked on validationRules
    Worked on the validation rules for the registration of users

--- validation_rules/register.js
+++ validation_rules/register.js
@@ -1,1 +1,16 @@
 const validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+module.exports = function validateRegistration(validate) {
+    let err = {};
+
+    if(!validator.isLength(validate.name, { min: 2, max: 30 })){
+        err.name = 'Name must be atleast 2 and max 30 characters long.'
+    }
+
+    return {
+        err,
+        isValid: isEmpty(err)
+    }
+
+}
