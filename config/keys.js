ded26f484c4db60e96396432fab73765bc4a21c0
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-10 22:11:11 +0200

    Refactor config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://user:secret123@ds153700.mlab.com:53700/social_network',
-    key: '17652174317GGJFdsdj&%('
-}
+  mongoURI: "mongodb://user:secret123@ds153700.mlab.com:53700/social_network",
+  key: "17652174317GGJFdsdj&%("
+};
--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,8 @@
-const jwtStrategy = require('passport-jwt').Strategy;
-const extractJwt = require('passport-jwt').ExtractJwt;
-const key = require('../config/keys').key;
-const User = require('../models/User');
-const db = require('../db/base');
+const jwtStrategy = require("passport-jwt").Strategy;
+const extractJwt = require("passport-jwt").ExtractJwt;
+const key = require("../config/keys").key;
+const User = require("../models/User");
+const db = require("../db/base");
 
 let opts = {};
 
@@ -10,17 +10,18 @@ opts.jwtFromRequest = extractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = key;
 
 module.exports = passport => {
-    passport.use(new jwtStrategy(opts, (jwt_payload, done) => {
-        db.connectMongoose();
-        User.findById(jwt_payload.id)
-            .then( user => {
-                if(!user) {
-                    db.disconnectMongoose();
-                    return done(null, false);
-                } else {
-                    db.disconnectMongoose();
-                    return done(null, user);
-                }
-            });
-    })); // END of passport use-function
-}
+  passport.use(
+    new jwtStrategy(opts, (jwt_payload, done) => {
+      db.connectMongoose();
+      User.findById(jwt_payload.id).then(user => {
+        if (!user) {
+          db.disconnectMongoose();
+          return done(null, false);
+        } else {
+          db.disconnectMongoose();
+          return done(null, user);
+        }
+      });
+    })
+  ); // END of passport use-function
+};
