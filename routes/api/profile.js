2bad030d5ab6d5730626f3a0ae50f84a0eab517a
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-15 04:08:06 +0200

    made some work on profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../../db/base");
+const passport = require("passport");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
 
-router.get('/placeholder', (req, res) => res.json({ msg: 'profileroutes works' }));
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const err = {};
+    db.connectMongoose();
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (!profile) {
+        err.noprofile = "This user has no profile. Yet.";
+        db.disconnectMongoose();
+        res.json(err);
+      }
+      db.disconnectMongoose();
+      res.json(profile);
+    });
+  }
+);
 
 module.exports = router;
