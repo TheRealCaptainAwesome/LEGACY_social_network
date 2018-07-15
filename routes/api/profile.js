7102c73e6f7eae54c595bc623b04e8e1909efa14
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-16 01:19:20 +0200

    First work on create a profile request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -24,4 +24,25 @@ router.get(
   }
 );
 
+router.get(
+  "/createprofile",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const profileData = {};
+    profileData.social_media = {};
+
+    profileData.user = req.user.id;
+    if (req.body.handle) profileData.handle = req.body.handle;
+    if (req.body.title) profileData.title = req.body.title;
+    if (req.body.location) profileData.location = req.body.location;
+    if (req.body.skills) profileData.skills = req.body.skills.split(",");
+    if (req.body.bio) profileData.bio = req.body.bio;
+    if (req.body.twitter) profileData.social_media.twitter = req.body.twitter;
+    if (req.body.facebook)
+      profileData.social_media.facebook = req.body.facebook;
+    if (req.body.instagram)
+      profileData.social_media.instagram = req.body.instagram;
+  }
+);
+
 module.exports = router;
