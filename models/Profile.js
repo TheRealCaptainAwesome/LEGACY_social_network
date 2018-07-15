92d9e89c273ad6a53540e7468b3d9a48529eddf5
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-15 03:21:36 +0200

    Created profile mongoose model

---
+++ models/Profile.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String
+  },
+  skills: {
+    type: [String]
+  },
+  bio: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      }
+    }
+  ],
+  social_media: {
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  }
+});
+
+module.exports = Profile = mongoose.model("Profile", profileSchema);
