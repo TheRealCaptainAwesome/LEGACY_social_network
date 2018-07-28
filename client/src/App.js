d2a1fe6babcd4d72f00f371f0cb2b368bdc01f85
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-07-28 22:59:41 +0200

    Added client folder + React
    Added the client folder and React within it.

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <p>base</p>;
+  }
+}
+
+export default App;
