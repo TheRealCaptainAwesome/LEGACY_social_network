a965331b3f88b6a60782e9a5efc95d6977191388
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-08-05 19:34:06 +0200

    Just saving progress: Navbar
    Saving some progress as I could not continue

--- client/src/components/containers/navbar/Navbar.js
+++ client/src/components/containers/navbar/Navbar.js
@@ -8,6 +8,10 @@ export default class Navbar extends Component {
         <div className="logoNtitle">
           <h1>Social Network</h1>
         </div>
+        <ul>
+          <li>Sign Up</li>
+          <li>Sign In</li>
+        </ul>
       </header>
     );
   }
