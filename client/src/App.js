0f8a30f193f93f6b32525f5ae26759ec692ffece
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-08-06 16:18:21 +0200

    Created main container

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 // Import containers
 import Navbar from "./components/containers/navbar/Navbar";
 import Footer from "./components/containers/footer/Footer";
+import Main from "./components/containers/main/Main";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Main />
         <Footer />
       </div>
     );
---
+++ client/src/components/containers/main/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main>
+        <p>Main content here</p>
+      </main>
+    );
+  }
+}
