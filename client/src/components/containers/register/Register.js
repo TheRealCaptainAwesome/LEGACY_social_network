024a8f4f6fadd6b729354329f19d1c273fbe2a26
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-08-12 21:44:51 +0200

    Added component state
    Added state for the register component
    
    Created a function that updates state based on input

--- client/src/components/containers/register/Register.js
+++ client/src/components/containers/register/Register.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
 
 export default class Register extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: ""
+  };
+
+  onInputChange = evt => {
+    const state = Object.assign({}, this.state);
+    state[evt.target.name] = evt.target.value;
+    this.setState(state);
+  };
+
   render() {
     return (
       <main>
         <form>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={this.state.name}
+            onChange={this.onInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={this.state.email}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={this.state.password}
+          />
           <input type="submit" />
         </form>
       </main>
