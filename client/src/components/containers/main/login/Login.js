efbcfefefd70b32a1cb7be6130645062c1d24dc5
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-08-14 21:03:42 +0200

    Added the login form
    Created the tests for the login form
    Coded the form

--- client/src/components/containers/main/login/Login.js
+++ client/src/components/containers/main/login/Login.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 
 export default class Register extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  onInputChange = evt => {
+    let state = Object.assign({}, this.state);
+    state[evt.target.name] = evt.target.value;
+    this.setState(state);
+  };
+
+  onSubmit = evt => {
+    evt.preventDefault();
+    console.log(evt);
+  };
+
   render() {
     return (
-      <form>
-        <input type="text" />
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={this.state.email}
+          onChange={this.onInputChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={this.state.password}
+          onChange={this.onInputChange}
+        />
         <input type="submit" />
       </form>
     );
--- client/src/components/containers/main/login/Login.test.js
+++ client/src/components/containers/main/login/Login.test.js
@@ -3,7 +3,7 @@ import { shallow } from "enzyme";
 
 import Login from "./Login";
 
-describe("Register component", () => {
+describe("Login component", () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(<Login />);
@@ -11,5 +11,25 @@ describe("Register component", () => {
 
   it("Renders a form", () => {
     expect(wrapper.find("form").exists()).toBe(true);
+  });
+
+  it("Contains email and password fields", () => {
+    expect(wrapper.find("input[name='email']").exists()).toBe(true);
+    expect(wrapper.find("input[name='password']").exists()).toBe(true);
+  });
+
+  it("Updates component state on user input", () => {
+    let emailInput = wrapper.find("input[name='email']");
+    emailInput.props().onChange({
+      target: { name: "email", value: "john@email.com" }
+    });
+
+    let passwordInput = wrapper.find("input[name='password']");
+    passwordInput.props().onChange({
+      target: { name: "password", value: "test123" }
+    });
+
+    expect(wrapper.state("email")).toBe("john@email.com");
+    expect(wrapper.state("password")).toBe("test123");
   });
 });
