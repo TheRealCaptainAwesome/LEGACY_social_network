77e85e57324931a668651df28f6c7620672211a4
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-09-01 18:09:40 +0200

    Created logout functionality

--- client/src/components/containers/navbar/Navbar.js
+++ client/src/components/containers/navbar/Navbar.js
@@ -1,26 +1,67 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+// Redux
+import { connect } from "react-redux";
+import { logOut } from "../../../redux/actions/authActions";
 
 import "./Navbar.css";
 
-export default class Navbar extends Component {
+class Navbar extends Component {
+  onLogout = e => {
+    e.preventDefault();
+    this.props.logOut();
+  };
+
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+    const loggedInLinks = (
+      <nav>
+        <ul>
+          <li>
+            <a href="" onClick={this.onLogout}>
+              Log Out
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+
+    const notLoggedInLinks = (
+      <nav>
+        <ul>
+          <li>
+            <Link to="/register">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/">Sign In</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+
     return (
       <header>
         <div className="logoNtitle">
           <h1>Social Network</h1>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/register">Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/">Sign In</Link>
-            </li>
-          </ul>
-        </nav>
+        {isAuthenticated ? loggedInLinks : notLoggedInLinks}
       </header>
     );
   }
 }
+
+Navbar.propTypes = {
+  logOut: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logOut }
+)(Navbar);
