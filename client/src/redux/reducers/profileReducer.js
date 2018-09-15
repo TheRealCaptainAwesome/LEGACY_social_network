f40d6f15a28ffaaa1ec3130146aecd6ec3cb3ed2
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-09-15 22:55:04 +0200

    Created profileReducer

---
+++ client/src/redux/reducers/profileReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_ERRORS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
