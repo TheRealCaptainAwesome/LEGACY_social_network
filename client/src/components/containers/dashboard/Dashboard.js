5013aa939b471a5a1f73cc3ffe2169c30acbd6aa
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-09-22 18:49:36 +0200

    removed a log-statement

--- client/src/components/containers/dashboard/Dashboard.js
+++ client/src/components/containers/dashboard/Dashboard.js
@@ -14,7 +14,6 @@ class Dashboard extends Component {
     const { profile, loading } = this.props.profile;
 
     let dashboardContent;
-    console.log(profile);
     if (profile === null || loading) {
       dashboardContent = <Loader />;
     } else if (!profile.noprofile) {
