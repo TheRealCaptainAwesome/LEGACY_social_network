b23182b660530375a954184816814d226cfcdc51
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-09-23 16:43:59 +0200

    Created createProfile component
    For now only base component checked in

---
+++ client/src/components/containers/createProfile/CreateProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class CreateProfile extends Component {
+  render() {
+    return (
+      <div>
+        <span />
+      </div>
+    );
+  }
+}
+
+export default connect()(CreateProfile);
