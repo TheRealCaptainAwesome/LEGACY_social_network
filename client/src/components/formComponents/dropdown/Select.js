8764b931fede347ba3a7bb8c667da564fd4cdf4e
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-09-23 18:23:12 +0200

    Created selectlist component

---
+++ client/src/components/formComponents/dropdown/Select.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SelectList = ({ name, value, onChange, options }) => {
+  const options = options.map(option => {
+    <option value={option}>{option}</option>;
+  });
+  return (
+    <select name={name} value={value} onChange={onChange}>
+      {options}
+    </select>
+  );
+};
+
+SelectList.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+};
+
+export default SelectList;
