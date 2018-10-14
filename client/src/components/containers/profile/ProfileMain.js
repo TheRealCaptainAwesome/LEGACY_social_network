014793c269c5710ed287be26a1d4519793fa913e
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-10-14 11:48:19 +0200

    Base structure for profileMain component

--- client/src/components/containers/profile/ProfileMain.js
+++ client/src/components/containers/profile/ProfileMain.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../../redux/utilities/isEmpty";
 
 class ProfileMain extends Component {
   render() {
-    return <div>TODO: PROFILEMAIN</div>;
+    const { profile } = this.props;
+
+    const skills = profile.skills.map((skill, index) => {
+      return <li key={index}>{skill}</li>;
+    });
+
+    return (
+      <div>
+        <div>
+          <h2>Bio</h2>
+          <p>{isEmpty(profile.bio) ? null : profile.bio}</p>
+        </div>
+        <div>
+          <h2>Skills</h2>
+          <ul>{skills}</ul>
+        </div>
+      </div>
+    );
   }
 }
 
