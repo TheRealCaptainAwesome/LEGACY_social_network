3f804e97fbe50b6a8946959a971ff2a28b687fdf
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-10-20 14:51:13 +0200

    Functionality to delete posts now works

--- client/src/components/containers/posts/PostItem.js
+++ client/src/components/containers/posts/PostItem.js
@@ -4,8 +4,13 @@ import { Link } from "react-router-dom";
 
 // Redux
 import { connect } from "react-redux";
+import { deletePost } from "../../../redux/actions/postActions";
 
 class PostItem extends Component {
+  onDeletePost = id => {
+    this.props.deletePost(id);
+  };
+
   render() {
     const { post, auth } = this.props;
 
@@ -17,8 +22,13 @@ class PostItem extends Component {
         </div>
         <div>
           <span>Likes: {post.likes.length}</span>
+          <Link to={`/post/${post._id}`}>Comments</Link>
         </div>
-        {post.user === auth.user.id ? <button>Delete Post</button> : null}
+        {post.user === auth.user.id ? (
+          <button onClick={this.onDeletePost.bind(this, post._id)}>
+            Delete Post
+          </button>
+        ) : null}
       </div>
     );
   }
@@ -26,11 +36,15 @@ class PostItem extends Component {
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  deletePost: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps)(PostItem);
+export default connect(
+  mapStateToProps,
+  { deletePost }
+)(PostItem);
