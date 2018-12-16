f7f2764cd538a5020c7e27989bfba972e3d9c72f
Jonas Stiernström <stiernstrom_3@hotmail.com>
2018-12-16 12:44:56 +0100

    Updated TextArea form component, now returns a div

--- client/src/components/formComponents/fields/TextArea.js
+++ client/src/components/formComponents/fields/TextArea.js
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 
 const TextArea = ({ name, placeholder, value, onChange }) => {
   return (
-    <textarea
-      placeholder={placeholder}
-      name={name}
-      value={value}
-      onChange={onChange}
-    />
+    <div>
+      <textarea
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
   );
 };
 
